820a207f8842bdc9698c9fadaffdfbd504d5d407
Kelvinmti <kdevweb@gmail.com>
2014-09-13 14:46:44 -0300

    Comitado

--- js/cursos.js
+++ js/cursos.js
@@ -1,30 +1,54 @@
-function carregarProvas(){
+function carregarCursos(){
 	banco.transaction( function (tx) {
-		//tx.executeSql("DROP TABLE disciplinas");
-		 tx.executeSql('CREATE TABLE IF NOT EXISTS disciplinas (codigo integer primary key autoincrement, nome, nota1, nota2)', [],
-			function () {	
-			 tx.executeSql( 'SELECT c.Nome, c.Duracao, c.Descricao FROM cursos c INNER JOIN Area a ON(a.Id = c.Id_Area) INNER JOIN Prof_Curso p ON(p.Id_Professor = c.Id_Area)', [],
+			tx.executeSql( 'SELECT c.* FROM Curso AS c', [],
 			        function (tx, results){    	  
 			          	var quant = results.rows.length;
 			          	var itens = [];
 			          	for (var i = 0; i < quant; i++)
 			          	{
-			           	  	var disc = results.rows.item(i);
-			           	  	var item = document.createElement('div');
-							item.setAttribute('data-bb-type','item');
-							item.setAttribute('data-bb-title',disc.nome);
-							item.innerHTML = "Nota1: " + disc.nota1;
-							item.setAttribute('id', disc.codigo);
-							item.setAttribute('onclick', '');
-							itens.push(item);
+			           	  	var res = results.rows.item(i);
+			           	  	var item = document.createElement('button');
+			           	  	var href = "document.location.href='cursos.detalhes.html'?id="+ res.Id;
+			           	  	item.setAttribute('type','button');
+								item.innerHTML = res.Nome;
+								item.setAttribute('id', res.Id);
+								item.setAttribute('class', 'btn btn-default btn-lg btn-block');
+								item.setAttribute('onclick', href);
+								itens.push(item);
+								$('#listaCursos').append(item);
 			            }
-			            document.getElementById('listaProvas').refresh(itens);   		          
 					},
 			        function (tx, error)
 			        {
 			         	alert('Ops.. ' + error.message);
-			        });
-			});
+					  });
+	});
+}
+
+function loadFromCourse(id){
+	banco.transaction( function (tx) {
+			tx.executeSql( 'SELECT c.* FROM Curso AS c INNER JOIN Area AS a ON(a.Id = c.Id_Area) INNER JOIN Grade AS g ON(g.Id_Curso = c.Id) LEFT JOIN Prof_Curso AS pc ON(pc.Id_Curso = c.Id) INNER JOIN Professor AS prof ON(prof.Id = pc.Id_Professor) WHERE c.Id = ' + id, [],
+			        function (tx, results){    	  
+			          	var quant = results.rows.length;
+			          	var itens = [];
+			          	for (var i = 0; i < quant; i++)
+			          	{
+			           	  	var res = results.rows.item(i);
+			           	  	var item = document.createElement('button');
+			           	  	var href = "document.location.href='cursos.detalhes.html'";
+			           	  	item.setAttribute('type','button');
+								item.innerHTML = res.Nome;
+								item.setAttribute('id', res.Id);
+								item.setAttribute('class', 'btn btn-default btn-lg btn-block');
+								item.setAttribute('onclick', href);
+								itens.push(item);
+								$('#listaCursos').append(item);
+			            }
+					},
+			        function (tx, error)
+			        {
+			         	alert('Ops.. ' + error.message);
+					  });
 	});
 
 }
