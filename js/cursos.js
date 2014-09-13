0c7a4fd2302ae6934543f0d322a96e28d2841fda
Kelvinmti <kdevweb@gmail.com>
2014-09-13 17:10:15 -0300

    Modificacoes

--- js/cursos.js
+++ js/cursos.js
@@ -1,3 +1,8 @@
+function loadPage(id) {
+	sessionStorage.idCurso = id;
+	document.location.href = 'cursos.detalhes.html';
+}
+
 function carregarCursos(){
 	banco.transaction( function (tx) {
 			tx.executeSql( 'SELECT c.* FROM Curso AS c', [],
@@ -8,12 +13,11 @@ function carregarCursos(){
 			          	{
 			           	  	var res = results.rows.item(i);
 			           	  	var item = document.createElement('button');
-			           	  	var href = "document.location.href='cursos.detalhes.html'?id="+ res.Id;
 			           	  	item.setAttribute('type','button');
 								item.innerHTML = res.Nome;
 								item.setAttribute('id', res.Id);
 								item.setAttribute('class', 'btn btn-default btn-lg btn-block');
-								item.setAttribute('onclick', href);
+								item.setAttribute('onclick', 'loadPage('+res.Id+')');
 								itens.push(item);
 								$('#listaCursos').append(item);
 			            }
@@ -25,24 +29,71 @@ function carregarCursos(){
 	});
 }
 
+function criarDivs() {
+
+}
+
 function loadFromCourse(id){
+	if (id !== 'undefined') {
 	banco.transaction( function (tx) {
-			tx.executeSql( 'SELECT c.* FROM Curso AS c INNER JOIN Area AS a ON(a.Id = c.Id_Area) INNER JOIN Grade AS g ON(g.Id_Curso = c.Id) LEFT JOIN Prof_Curso AS pc ON(pc.Id_Curso = c.Id) INNER JOIN Professor AS prof ON(prof.Id = pc.Id_Professor) WHERE c.Id = ' + id, [],
+			tx.executeSql( 'SELECT c.*, a.Nome AS area FROM Curso AS c INNER JOIN Area AS a ON(a.Id = c.Id_Area) WHERE c.Id = ' + id, [],
 			        function (tx, results){    	  
 			          	var quant = results.rows.length;
-			          	var itens = [];
 			          	for (var i = 0; i < quant; i++)
 			          	{
 			           	  	var res = results.rows.item(i);
-			           	  	var item = document.createElement('button');
-			           	  	var href = "document.location.href='cursos.detalhes.html'";
-			           	  	item.setAttribute('type','button');
+			           	  	if (i == 0) {
+			           	  		$('#title').append(res.Nome);		           	  		
+			           	  	} else {
+			           	  		
+			           	  	}
+
+			           	  	item = document.createElement('div');
+								item.innerHTML = "Area: " + res.area;
+								item.setAttribute('class', 'col-md-12');
+								$('#detalhe').append(item);
+
+			           	  	/*item = document.createElement('div');
+								item.innerHTML = "Nome: " + res.Nome;
+								item.setAttribute('class', 'col-md-12');
+								$('#detalhe').append(item);*/
+
+			           	  	item = document.createElement('div');
+								item.innerHTML = "Duração: " + res.Duracao;
+								item.setAttribute('class', 'col-md-12');
+								$('#detalhe').append(item);
+
+								item = document.createElement('div');
+								item.innerHTML = "Descrição: ";
+								item.setAttribute('class', 'col-md-12');
+								$('#detalhe').append(item);
+
+								item = document.createElement('div');
+								item.innerHTML = res.Descricao;
+								item.setAttribute('class', 'col-md-12');
+								$('#detalhe').append(item);
+			            }
+					},
+			        function (tx, error)
+			        {
+			         	alert('Ops.. ' + error.message);
+					  });
+
+        //PROFESSOR 
+        tx.executeSql( 'SELECT prof.* FROM Prof_Curso AS pc INNER JOIN Professor AS prof ON(prof.Id = pc.Id_Professor) WHERE pc.Id_Curso = ' + id, [],
+			        function (tx, results){    	  
+			          	var quant = results.rows.length;
+		          	   var item = document.createElement('div');
+							item.innerHTML = "Corpo docente: ";
+							item.setAttribute('class', 'col-md-12');
+							$('#detalhe').append(item);
+			          	for (var i = 0; i < quant; i++)
+			          	{
+			           	  	var res = results.rows.item(i);			           	  	
+			           	  	item = document.createElement('div');
 								item.innerHTML = res.Nome;
-								item.setAttribute('id', res.Id);
-								item.setAttribute('class', 'btn btn-default btn-lg btn-block');
-								item.setAttribute('onclick', href);
-								itens.push(item);
-								$('#listaCursos').append(item);
+								item.setAttribute('class', 'col-md-12');
+								$('#detalhe').append(item);
 			            }
 					},
 			        function (tx, error)
@@ -50,5 +101,5 @@ function loadFromCourse(id){
 			         	alert('Ops.. ' + error.message);
 					  });
 	});
-
+}
 }
